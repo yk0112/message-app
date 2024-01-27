d50ee3ed539fe743009348a610e587303a6682bb
yk0112 <yosikou0726@gmail.com>
2024-01-28 01:09:32 +0900

    feat: formコンポーネントの外装

--- message-app/app/(site)/components/AuthForm.tsx
+++ message-app/app/(site)/components/AuthForm.tsx
@@ -4,6 +4,9 @@ import { handleClientScriptLoad } from "next/script";
 import { useCallback, useEffect, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import Input from "../../../components/inputs/Input";
+import Button from "../../../components/inputs/Button";
+import AuthSocialButton from "../../../components/inputs/AuthSocialButton";
+import { BsGithub, BsGoogle } from "react-icons/bs";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -47,23 +50,90 @@ const AuthForm = () => {
   return (
     <div
       className="
-    	mt-8 
+    	mt-6
     	sm:mx-auto 
     	sm:w-full 
     	sm:max-w-md"
     >
       <div
         className="
-   	  bg-white
-   	  px-4
-   	  py-8
-   	  shadow
-   	  sm:rounded-lg
-   	  sm:px-10"
+       bg-white
+       px-4
+       py-8
+       shadow
+       sm:rounded-lg
+       sm:px-10"
       >
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-          <Input id="email" label="Email" register={register} errors={errors} />
+          {variant == "REGISTER" && (
+            <Input id="name" label="Name" register={register} errors={errors} />
+          )}
+          <Input
+            id="email"
+            label="Email address"
+            register={register}
+            errors={errors}
+          />
+          <Input
+            id="password"
+            label="Password"
+            register={register}
+            errors={errors}
+          />
+          <div>
+            <Button disabled={isLoading} fullwidth type="submit">
+              {variant == "LOGIN" ? "Sign in" : "Register"}
+            </Button>
+          </div>
         </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div
+              className="
+                absolute 
+                inset-0 
+                flex 
+                items-center
+              "
+            >
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction("github")}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction("google")}
+            />
+          </div>
+          <div
+            className="
+	     flex
+	     gap-2
+	     justify-center 
+	     text-sm mt-6 px-2 
+	     text-gray-500
+            "
+          >
+            <div>
+              {variant == "LOGIN"
+                ? "New to message-app?"
+                : "Already have an account?"}
+            </div>
+            <div onClick={toggleVariant} className="underline cursor-pointer">
+              {variant == "LOGIN" ? "Create an account" : "Login"}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
