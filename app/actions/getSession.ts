621c3d3f78254d07c5e2d6adf0ba98b1fd273239
yk0112 <yosikou0726@gmail.com>
2024-02-24 00:56:59 +0900

    debug

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/libs/auth";
 
 export default async function getSession() {
   return await getServerSession(authOptions);
